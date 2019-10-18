d8aef35c29a94150e2fea8a5bd4e557487cebb2e
AzharSultan <azh.sultan@gmail.com>
2019-10-18 15:21:46 +0200

    Update segmentation_dataset.py

--- research/deeplab/deprecated/segmentation_dataset.py
+++ research/deeplab/deprecated/segmentation_dataset.py
@@ -14,37 +14,27 @@
 # ==============================================================================
 
 """Provides data from semantic segmentation datasets.
-
 The SegmentationDataset class provides both images and annotations (semantic
 segmentation and/or instance segmentation) for TensorFlow. Currently, we
 support the following datasets:
-
 1. PASCAL VOC 2012 (http://host.robots.ox.ac.uk/pascal/VOC/voc2012/).
-
 PASCAL VOC 2012 semantic segmentation dataset annotates 20 foreground objects
 (e.g., bike, person, and so on) and leaves all the other semantic classes as
 one background class. The dataset contains 1464, 1449, and 1456 annotated
 images for the training, validation and test respectively.
-
 2. Cityscapes dataset (https://www.cityscapes-dataset.com)
-
 The Cityscapes dataset contains 19 semantic labels (such as road, person, car,
 and so on) for urban street scenes.
-
 3. ADE20K dataset (http://groups.csail.mit.edu/vision/datasets/ADE20K)
-
 The ADE20K dataset contains 150 semantic labels both urban street scenes and
 indoor scenes.
-
 References:
   M. Everingham, S. M. A. Eslami, L. V. Gool, C. K. I. Williams, J. Winn,
   and A. Zisserman, The pascal visual object classes challenge a retrospective.
   IJCV, 2014.
-
   M. Cordts, M. Omran, S. Ramos, T. Rehfeld, M. Enzweiler, R. Benenson,
   U. Franke, S. Roth, and B. Schiele, "The cityscapes dataset for semantic urban
   scene understanding," In Proc. of CVPR, 2016.
-
   B. Zhou, H. Zhao, X. Puig, S. Fidler, A. Barriuso, A. Torralba, "Scene Parsing
   through ADE20K dataset", In Proc. of CVPR, 2017.
 """
@@ -75,6 +65,15 @@ DatasetDescriptor = collections.namedtuple(
                       # VOC 2012 dataset. Thus, we set num_classes=21.
      'ignore_label',  # Ignore label value.
     ]
+)
+
+_XVIEW2_INFORMATION = DatasetDescriptor(
+    splits_to_sizes={
+        'train': 2235,
+        'val': 564,
+    },
+    num_classes=2,
+    ignore_label=255,
 )
 
 _CITYSCAPES_INFORMATION = DatasetDescriptor(
@@ -113,6 +112,7 @@ _DATASETS_INFORMATION = {
     'cityscapes': _CITYSCAPES_INFORMATION,
     'pascal_voc_seg': _PASCAL_VOC_SEG_INFORMATION,
     'ade20k': _ADE20K_INFORMATION,
+    'xview2': _XVIEW2_INFORMATION
 }
 
 # Default file pattern of TFRecord of TensorFlow Example.
@@ -125,15 +125,12 @@ def get_cityscapes_dataset_name():
 
 def get_dataset(dataset_name, split_name, dataset_dir):
   """Gets an instance of slim Dataset.
-
   Args:
     dataset_name: Dataset name.
     split_name: A train/val Split name.
     dataset_dir: The directory of the dataset sources.
-
   Returns:
     An instance of slim Dataset.
-
   Raises:
     ValueError: if the dataset_name or split_name is not recognized.
   """
